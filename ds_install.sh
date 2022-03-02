4748d1ad4acdf9d276009c4f3dff142a824e2d3f
Sinki25 <ms.sinkovets@mail.ru>
2022-03-02 18:01:01 +0300

    Troubleshooting password change

--- ds_install.sh
+++ ds_install.sh
@@ -19,7 +19,7 @@ audit_database_password=${18}
 audit_server_name=${19}
 ds_server_name=${20}
 key_vault_name=${21}
-ds_admin_password=`az keyvault secret show --name dsSecretAdminPassword --vault-name $key_vault_name --query value --output tsv`
+#ds_admin_password=`az keyvault secret show --name dsSecretAdminPassword --vault-name $key_vault_name --query value --output tsv`
 echo $ds_admin_password
 ds_remove_servers="/var/lib/waagent/custom-script/download/1/${22}"
 ds_license=${23}
@@ -62,7 +62,7 @@ logBeginAct "Pre_setup execution"
 
 install_product $link_to_DS_build
 
-az keyvault secret show --name dsSecretAdminPassword --vault-name $key_vault_name --query value --output tsv
+ds_admin_password=`az keyvault secret show --name dsSecretAdminPassword --vault-name $key_vault_name --query value --output tsv`
 
 RETVAL=$?
 
